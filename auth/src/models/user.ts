440870deb6376183e32e6af411d0c4c5e04dfd29
Behrooz Tabesh <Behrooztabesh@gmail.com>
2021-11-28 19:58:17 -0800

    Add mongoose and hashing password

---
+++ auth/src/models/user.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose'
+import { Password } from '../services/password'
+
+// An interface that describes the properties that are required to create a new user
+// (What it take to create a user)
+interface UserAttrs {
+  email: string
+  password: string
+}
+
+// An interface that describe the properties that User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+// An interface that describes the properties that user document has
+// (What Propertues a single user have)
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+  // createdAt: string
+  // updateAt: string
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.pre('save', async function (done) {
+  // if only password is modified not the email
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+export { User }
